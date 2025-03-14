05361c4720876b2e02b53d6f8348c9c2cece6720
Noelia <Noelia.noe@gmail.com>
2025-03-14 20:47:22 +0100

    14/03/2025

--- index.php
+++ index.php
@@ -3,9 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registro</title>
+    <title>Autenticación</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    
+    <header class='logo'>LOGO</header>
+    <nav class='navegador'></nav>
+    <main class='contenido'>
+        <!-- From Uiverse.io by Praashoo7 --> 
+        <form class="form">
+            <p class="heading">LOGIN</p>
+            <input placeholder="Username" class="input" type="text">
+            <input placeholder="Password" class="input" type="password">
+            <button class="btn">Submit</button>
+        </form>
+    </main>
+    <footer class='pie'></footer>
 </body>
 </html>
